38338766e7896383edc0249e220e22cfd5707f04
dondong349 <349538197@qq.com>
2017-05-05 21:03:03 +0800

    Merge remote-tracking branch 'github/master'

--- src/core/component/view/BaseComponent.ts
+++ src/core/component/view/BaseComponent.ts
@@ -1,36 +1,181 @@
-module hc {
-	export class BaseComponent extends eui.Component{
+module hc
+{
+	/**
+	 *
+	 * @desc 基本的UI界面显示类
+	 *
+	 */
+	export class UIComponent extends eui.Component
+	{
 		private _data:any;
-		public constructor() {
+		public eventDic: any;
+		private touchBeginTaret:any;
+		protected isSkinLoaded:boolean;
+		public constructor()
+		{
 			super();
-			this.addEventListener(eui.UIEvent.CREATION_COMPLETE,this.onCompleted,this);
+			this.eventDic = {};
+			this.addEventListener(eui.UIEvent.CREATION_COMPLETE, this.onCreationComplete, this);
 		}
-		/** 创建完成 */
-		private onCompleted():void{
-			this.removeEventListener(eui.UIEvent.CREATION_COMPLETE,this.onCompleted,this);
+
+		/**
+		 * 组件初始化完毕
+		 */
+		protected onCreationComplete(evt: eui.UIEvent): void
+		{
+			this.initComponent();
+			this.initData();
+			this.initListener();
+			this.isSkinLoaded=true;
+			this.removeEventListener(eui.UIEvent.CREATION_COMPLETE, this.onCreationComplete, this);
 		}
-		public onOpen():void{
-			
-		}
-		/** 初始化界面 */
-		public initUI():void{
+		/**
+		 * 初始化组件,需被子类继承
+		 */
+		protected initComponent(): void
+		{
 
 		}
-		/** 初始化事件 */
-		public initEvent():void{
+		/**
+		 * 初始化数据
+		 */
+		protected initData(): void
+		{
+			this.onStageResize();
+		}
+
+		/**
+		 * @description 获取当前属于这个模块的数据
+		 */
+		public get data():any{
+			return this._data;
+		}
+		/**
+		 * @description 获取当前属于这个模块的数据
+		 */
+		public set data(data:any){
+			this._data = data;
+		}
+		/**
+		 * 初始化事件监听器,需被子类继承
+		 */
+		protected initListener(): void
+		{
+			//this.addEventListener(egret.Event.RESIZE, this.onStageResize, this);
+			lemon.NotifyManager.registerNotify(lemon.StageUtil.STAGE_RESIZE, this.onStageResize, this);
+		}
+		protected onStageResize(){
 
 		}
-		/** 初始化数据 */
-		public initData(data):void{
-
+		/**
+		 * 事件注册，所有事件的注册都需要走这里
+		 */
+		public addEvent(target:egret.EventDispatcher,type:string,callBack:Function,thisObject:any):void{
+			var eventParams: any = {};
+			eventParams.target = target;
+			eventParams.type = type;
+			eventParams.callBack = callBack;
+			eventParams.thisObject = thisObject;
+			if(target) {
+				target.addEventListener(type,callBack,thisObject);
+				this.eventDic[target.hashCode + type]=eventParams;
+			}
 		}
-		/** 屏幕自适应 */
-		public onResize():void{
-
+		/**
+		 * @description 添加点击函数
+		 */
+		protected addClickEvent(target:egret.EventDispatcher,callBack:Function,thisObject:any):void{
+			var eventParams: any = {};
+			eventParams.target = target;
+			eventParams.type = egret.TouchEvent.TOUCH_BEGIN;
+			if(target) {
+				target.addEventListener(eventParams.type,this.onTouchBegin,this);
+				this.eventDic[target.hashCode + eventParams.type] = eventParams;
+			}
+			var eventParamsEnd: any = {};
+			eventParamsEnd.target = target;
+			eventParamsEnd.type = egret.TouchEvent.TOUCH_END;
+			eventParamsEnd.callBack = callBack;
+			eventParamsEnd.thisObject = thisObject;
+			if(target) {
+				target.addEventListener(eventParamsEnd.type,this.onTouchEnd,this);
+				this.eventDic[target.hashCode + eventParamsEnd.type] = eventParamsEnd;
+			}
+			var eventParamsOutSide: any = {};
+			eventParamsOutSide.target = target;
+			eventParamsOutSide.type = egret.TouchEvent.TOUCH_RELEASE_OUTSIDE;
+			if(target) {
+				target.addEventListener(eventParamsOutSide.type,this.onTouchReleaseOutSide,this);
+				this.eventDic[target.hashCode + eventParamsOutSide.type] = eventParamsOutSide;
+			}
 		}
-		/** closed */
-		public onClose():void{
-			if(this.parent) this.parent.removeChild(this);
+		/**
+		 * @description 当点击开始
+		 */
+		private onTouchBegin(evt:egret.TouchEvent):void{
+			this.touchBeginTaret = evt.target;
+			egret.Tween.get(evt.target).to({scaleX:0.9,scaleY:0.9},50);
+		}
+		/**
+		 * @description 当点击结束
+		 */
+		private onTouchEnd(evt:egret.TouchEvent):void{
+			let self:any=this;
+			let target:any=evt.target;
+			if(this.touchBeginTaret != target) return;
+			this.touchBeginTaret = null;
+			egret.Tween.get(target).to({ scaleX: 1,scaleY:1 },50).call(function(){
+				for(let key in self.eventDic) {
+					let eventParams: any = self.eventDic[key];
+					if(eventParams.target == target && eventParams.type == egret.TouchEvent.TOUCH_END) {
+						eventParams.callBack.call(eventParams.thisObject);
+					}
+				}
+			},this);
+		}
+		/**
+		 * @description 当点击结束的时候，按钮不在被点击的对象上
+		 */
+		private onTouchReleaseOutSide(evt:egret.TouchEvent):void{
+			if(this.touchBeginTaret != evt.target) return;
+			this.touchBeginTaret = null;
+			evt.target.scaleX=1;
+			evt.target.scaleY=1;
+		}
+		/**
+		 * 统一移除所有事件
+		 */
+		protected removeAllEvent(): void
+		{
+			let tempEvent;
+			for(let name in this.eventDic) {
+				tempEvent = this.eventDic[name];
+				if (tempEvent.target != null)
+				{
+					tempEvent.target.removeEventListener(tempEvent.type, tempEvent.callBack, tempEvent.thisObject);
+				}
+				delete this.eventDic[name];
+			}
+			this.eventDic = {};
+		}
+		/**
+		 * 资源释放
+		 * @$isDispos 是否彻底释放资源
+		 */
+		public dispos($isDispos: boolean = false): void
+		{
+			lemon.NotifyManager.removeThisObjectNofity(this);
+			this.removeAllEvent();
+			this.touchBeginTaret = null;
+			this._data = null;
+			if ($isDispos)
+			{
+				//todo释放资源
+			}
+			if (this.parent)
+			{
+				this.parent.removeChild(this);
+			}
 		}
 	}
 }
--- src/core/component/view/BaseView.ts
+++ src/core/component/view/BaseView.ts
@@ -1,7 +1,121 @@
 module hc {
-	export class BaseView extends BaseComponent{
+	/**
+	 *
+	 * @author 帅波
+	 * @date 2016.12.05
+	 * @description 所有模块的基类
+	 *
+	 */
+	export class BaseModule extends UIComponent{
+		/**
+		 * _data: {
+		 * 		index:number   打开第几个子界面
+		 * }
+		 */
+		private _isShow:boolean;
+		private _qmrSkinName:string;
+		public  resName:string;      //模块所引用的资源组
+
+		//采用 addClickEvent 监听的控件，必须要触发了touchBegin才能做touchEnd
+		//private touchBeginTaret;
 		public constructor() {
 			super();
+		}
+
+		/**
+		 * 组件初始化完毕
+		 */
+		protected onCreationComplete(evt: eui.UIEvent): void
+		{
+			super.onCreationComplete(evt);
+		}
+		protected onStageResize(evt?:egret.Event):void{
+			this.width = lemon.StageUtil.stageWidth;
+			this.height = lemon.StageUtil.stageHeight;
+		}
+		/**
+		 * @description 设置皮肤名字,需要在构造函数里面调用
+		 */
+		public set qmrSkinName(value:string){
+			this._qmrSkinName=value;
+		}
+		/**
+		 * @description 初始化组件,需被子类继承
+		 */
+		protected initComponent():void{
+		}
+		/**
+		 * @description 初始化事件,需被子类继承
+		 */
+		protected initListener():void{
+			super.initListener();
+		}
+		/**
+		 * @description 初始化数据,需被子类继承
+		 */
+		protected initData():void{
+			super.initData();
+		}
+		/**
+		 * @description 打开模块
+		 * @param needLoadSkin 是否需要加载皮肤，这个在模块管理器ModuleManager里面自动判断了
+		 * @param data 打开模块时，需要向这个模块传递的一些数据
+		 * @param layer 当前模块所处的层级
+		 */
+		public openModule(needLoadSkin:boolean,data:any,layer:number):void{
+			this.data = data;
+			if(needLoadSkin){
+				this.skinName = this._qmrSkinName;
+			}else{
+				this.initData();
+				this.initListener();
+			}
+			this.show(layer);
+		}
+		/**
+		 * @description 显示模块
+		 */
+		private show(layer:number,zIndex?:number):void{
+			this._isShow = true;
+			//this.height = lemon.StageUtil.stageHeight;
+			LayerManager.getInstance().addDisplay(this,layer);
+		}
+		/**
+		 * @description 获取当前模块的显示状态
+		 */
+		public get isShow():boolean{
+			return this._isShow;
+		}
+		/**
+		 * @description 获取模块中某个控件在舞台中的位置
+		 */
+		public getComponentGlobalPoint(componentName:string):any{
+			let component:egret.DisplayObject=this[componentName];
+			if(component){
+				if(component.parent){
+					return component.parent.localToGlobal(component.x,component.y);
+				}else{
+					return component.localToGlobal(component.x,component.y);
+				}
+			}
+			return {x:0,y:0};
+		}
+		/**
+		 * @description 资源释放
+		 */
+		public dispos():void{
+			lemon.NotifyManager.removeThisObjectNofity(this);
+			for(let key in this.eventDic){
+				let eventParams:any=this.eventDic[key];
+				if(eventParams.target){
+					eventParams.target.removeEventListener(eventParams.type,eventParams.callBack,eventParams.thisObject);
+				}
+				delete this.eventDic[key];
+			}
+			this._isShow=false;
+			if(this.parent){
+				this.parent.removeChild(this);
+			}
 		}
 	}
 }
