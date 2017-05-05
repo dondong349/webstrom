c4b649f829d805382ecbac020ddeedf9041bd5fc
dondong349 <349538197@qq.com>
2017-05-05 11:27:46 +0800

    GameDemo
    first gameDemo
    
    Signed-off-by: dondong349 <349538197@qq.com>

--- src/Main.ts
+++ src/Main.ts
@@ -1,5 +1,5 @@
 //////////////////////////////////////////////////////////////////////////////////////
-//111111
+//
 //  Copyright (c) 2014-present, Egret Technology.
 //  All rights reserved.
 //  Redistribution and use in source and binary forms, with or without
--- src/core/manager/NotifyMgr.ts
+++ src/core/manager/NotifyMgr.ts
@@ -1,19 +1,79 @@
 module hc {
 	export class NotifyMgr {
-		private static _ins:NotifyMgr;
-		private notify:any;
+		private static typeDic:any={};
 		public constructor() {
-			this.notify = {};
 		}
-		public static getIns():NotifyMgr{
-			if(!this._ins) this._ins = new NotifyMgr();
-			return this._ins;
+		/**
+		 * @description 注册一个消息
+		 * @param type 消息类型
+		 * @param callBack 回调函数
+		 * @param thisObject 当前作用域对象
+		 */
+		public static registerNotify(type:string,callBack:Function,thisObject:any):void{
+			if(!NotifyManager.typeDic[type]){
+				NotifyManager.typeDic[type]=[{callback:callBack,thisObject:thisObject}];
+			}else{
+				let typeList: Array<any> = NotifyManager.typeDic[type];
+				let result:boolean=false;
+				for(let item of typeList){
+					if(item.callback == callBack && item.thisObject==thisObject){
+						result=true;
+						break;
+					}
+				}
+				if(!result){
+					typeList.push({ callback: callBack,thisObject: thisObject });
+					NotifyManager.typeDic[type] = typeList;
+				}
+			}
 		}
-
-		public addNotify(type:string,target:any,callBack:Function):void{
-			let callAndTarget = this.notify[type];
-			if(!callAndTarget) this.notify[type] = {};
-			if(this.notify[type] && this.notify[type])
+		/**
+		 * @description 取消一个注册消息
+		 * @param type 消息类型
+		 * @param callBack 回调函数
+		 * @param thisObject 当前作用域对象
+		 */
+		public static unRegisterNotify(type: string,callBack: Function,thisObject: any):void{
+			if(NotifyManager.typeDic[type]){
+				let typeList: Array<any> = NotifyManager.typeDic[type];
+				for(let item of typeList) {
+					if(item.callback == callBack && item.thisObject == thisObject) {
+						let index:number = typeList.indexOf(item);
+						if(index!=-1){
+							typeList.splice(index,1);
+						}
+						break;
+					}
+				}
+				NotifyManager.typeDic[type] = typeList;
+			}
+		}
+		/**
+		 * @description 发送一个消息通知
+		 */
+		public static sendNotification(type:string,params:any=null):void{
+			if(NotifyManager.typeDic[type]){
+				let typeList: Array<any> = NotifyManager.typeDic[type];
+				for(let item of typeList) {
+					if(item.callback){
+						item.callback.call(item.thisObject,params);
+					}
+				}
+			}
+		}
+		/**
+		 * @description 移除对应thisObject的所有消息
+		 */
+		public static removeThisObjectNofity(thisObject:any):void{
+			for(let type in NotifyManager.typeDic){
+				let typeList: Array<any> = NotifyManager.typeDic[type];
+				for(let i:number=typeList.length-1;i>=0;i--) {
+					if(typeList[i].thisObject == thisObject) {
+						typeList.splice(i,1);
+					}
+				}
+				NotifyManager.typeDic[type]=typeList;
+			}
 		}
 	}
 }
